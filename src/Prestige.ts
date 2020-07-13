b55985f79915de8a1786af425720377fd296a147
Zac Olden <zacolden@gmail.com>
2020-07-14 01:19:21 +1000

    Add CLI formatting

--- src/Prestige.ts
+++ src/Prestige.ts
@@ -1,8 +1,13 @@
 import PrestigeClient from './PrestigeClient';
+import chalk from 'chalk';
 
 class Prestige {
     constructor(private token: string, public client: PrestigeClient) {
-        client.login(token);
+        console.log(`Prestige ${chalk.gray(' [prestigebot.com] ')}\n`);
+        console.log('login -> attempting');
+        client.login(token)
+            .then(() => console.log(`login -> success`))
+            .catch(() => console.error(`login -> error`));
     }
 }
 
