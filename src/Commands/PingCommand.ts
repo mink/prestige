a31b65595daa33f0d50f22bc972b684263ca94ea
Zac Olden <zacolden@gmail.com>
2020-07-20 17:54:57 +1000

    Remove middleware from ping command

--- src/Commands/PingCommand.ts
+++ src/Commands/PingCommand.ts
@@ -1,9 +1,7 @@
 import Command from './Command';
 import { Message } from 'discord.js';
-import ownerOnly from './Middleware/ownerOnly';
 
 class PingCommand extends Command {
-    @ownerOnly
     run(message: Message) {
         return message.reply('pong');
     }
