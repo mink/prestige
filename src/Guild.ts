6f399bffd14060886dd69032fc6aff143fb44243
Zac Olden <zacolden@gmail.com>
2020-07-22 00:09:35 +1000

    Add instanced command handler

---
+++ src/Guild.ts
@@ -0,0 +1,21 @@
+import { CommandoClient, CommandoRegistry } from 'discord.js-commando';
+import CommandDispatcher from 'discord.js-commando/src/dispatcher';
+import PingCommand from './Commands/PingCommand';
+
+class Guild {
+    public dispatcher: CommandDispatcher;
+    public registry: CommandoRegistry;
+
+    constructor(client: CommandoClient) {
+        this.registry = new CommandoRegistry(client);
+        this.dispatcher = new CommandDispatcher(client, this.registry);
+        this.bindDefaultCommands();
+    }
+
+    bindDefaultCommands(): void {
+        this.registry.registerGroup('default');
+        this.registry.registerCommand(PingCommand);
+    }
+}
+
+export default Guild;
--- src/PrestigeClient.ts
+++ src/PrestigeClient.ts
@@ -1,15 +1,23 @@
-import PingCommand from './Commands/PingCommand';
 import { CommandoClient, CommandoClientOptions } from 'discord.js-commando';
+import Guild from './Guild';
 
 class PrestigeClient extends CommandoClient {
+
+    public prestigeGuilds: Map<string, Guild> = new Map<string, Guild>();
+
     constructor(options?: CommandoClientOptions) {
         super(options);
-        this.registry.registerGroup('default');
-        this.registry.registerCommand(PingCommand);
+        this.bindDefaultCommandHandler();
     }
 
     public login(token: string): Promise<string> {
         return super.login(token);
+    }
+
+    private bindDefaultCommandHandler(): void {
+        this.prestigeGuilds.set('default', new Guild(this));
+        this.registry = this.prestigeGuilds.get('default').registry;
+        this.dispatcher = this.prestigeGuilds.get('default').dispatcher;
     }
 }
 
