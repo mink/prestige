c833c1a6fb9e33d2ab221e53a804eaa4a74e761f
Zac Olden <zacolden@gmail.com>
2020-07-28 22:30:58 +1000

    Clean up owner only middleware

--- src/Commands/Middleware/ownerOnly.ts
+++ src/Commands/Middleware/ownerOnly.ts
@@ -6,10 +6,10 @@ function ownerOnly(
     descriptor: TypedPropertyDescriptor<any>
 ): TypedPropertyDescriptor<any> {
     let originalMethod = descriptor.value;
-    descriptor.value = function (...args: any[]) {
+    descriptor.value = (...args: any[]) => {
         const message = args[0];
         if (message instanceof Message && message.author.id !== process.env.CLIENT_OWNER) {
-            return args[0].channel.send(`You don't have permission to use this command.`);
+            return message.channel.send(`You don't have permission to use this command.`);
         }
         return originalMethod.apply(this, args);
     };
